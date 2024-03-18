3c6bacbbe8dbf796164888b69734ed9838e1fab4
Adelaide2510 <162982972+Adelaide2510@users.noreply.github.com>
2024-03-18 12:06:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit as st
-name=st.text_input("Your Name")
-st.write("Hello "+name)
+import pandas as pd
+import numpy as np
+voc=pd.nead_csv('lien')
+st.dataframe(voc)
+l=voc.shape[o]
+i=hp.random.choice(range(l))
+word_fr=voc['Définition'].values[i]
+word_chi=voc['?'].values[i]
+st.write(word_frt"hanzi"+word_chi)
+st.button("refresh")
